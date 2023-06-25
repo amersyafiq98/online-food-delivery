285e824ea1fbd3f38b0940f9132c9ddb8a7a338f
airynrinny <103765853+airynrinny@users.noreply.github.com>
2023-06-25 21:54:17 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '/homepage.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Initialize Firebase
